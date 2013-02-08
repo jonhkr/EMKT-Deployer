bace9957948e8ee1c770b0c1a5fde3a3182f99a2
Jonas Trevisan <jonhkr@gmail.com>
2013-02-08 11:46:34 -0200

    sending to ftp

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 EMKT-Deployer
 =============
 
-Upload to FTP and litmus your email marketing
+This project is under heavy development. And may have bugs :)
+
+Not easy to install yet, run setup to install requirements, or install using `pip`
+`pip install ftputil`
+
+then you can copy `__init__.py` to your `/usr/bin` folder rename it as you want and run `chmod +w NNN` to make it a executable
--- emktdeployer/__init__.py
+++ emktdeployer/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Upload to FTP and litmus your email marketing.')
@@ -20,6 +22,8 @@ prefix = foldersplit[0].upper()
 year = foldersplit[1]
 emktid = '-'.join(foldersplit[2:]).lower()
 
+folders = [prefix, year, 'emkt', emktid]
+
 from ConfigParser import ConfigParser
 
 config = ConfigParser()
@@ -29,6 +33,7 @@ with open(args.config) as f:
 
 from ftputil import FTPHost
 
+httphost = config.get('FTP', 'httphost')
 ftphost = config.get('FTP', 'host')
 ftpuser = config.get('FTP', 'username')
 ftppasswd = config.get('FTP', 'password')
@@ -36,13 +41,39 @@ ftppasswd = config.get('FTP', 'password')
 ftp = FTPHost(ftphost, ftpuser, ftppasswd)
 ftp.chdir('/')
 
-# TODO: Clean this up
-if ftp.path.exists(prefix):
-  if ftp.path.isdir(prefix):
-    ftp.chdir(prefix)
-    
+
+for f in folders:
+  if ftp.path.exists(f):
+    if not ftp.path.isdir(f):
+      raise Exception('%s exists on ftp server, but is not a directory' % f)
+    print('Dir exists %s' % f)
   else:
-    raise Exception('Prefix exists but is not a directory')
-else:
-  print('Creating dir %s' % prefix)
-  ftp.mkdir(prefix)
+    print('Creating dir %s' % f)
+    ftp.mkdir(f)
+  ftp.chdir(f)
+  print('chdir to %s' % f)
+
+print 'Starting upload in dir %s' % ftp.getcwd()
+
+# Start uploding
+import os
+
+def uploadfiles(localpath, ftppath):
+  for entry in os.listdir(localpath):
+    localentrypath = '%s/%s' % (localpath, entry)
+    ftpentrypath = '%s/%s' % (ftppath, entry)
+    if os.path.isdir(localentrypath):
+      if ftp.path.exists(ftpentrypath):
+        if not ftp.path.isdir(ftpentrypath):
+          raise Exception('%s exists on ftp server, but is not a directory' % ftpentrypath)
+      else:
+        print 'Creating dir %s' % ftpentrypath
+        ftp.mkdir(ftpentrypath)
+      uploadfiles(localentrypath, ftpentrypath)
+      continue
+    print('Uploading file %s' % ftpentrypath)
+    ftp.upload_if_newer(localentrypath, ftpentrypath)
+
+uploadfiles(folder.rstrip('/'), ftp.getcwd())
+
+print('Upload done: %s%s' % (httphost, ftp.getcwd()))
